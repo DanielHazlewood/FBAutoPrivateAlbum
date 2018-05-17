fe7481079be687659f6e9b70e1b1fa643e4649dc
Dan <34265576+DanielHazlewood@users.noreply.github.com>
2018-05-18 06:59:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,10 +24,11 @@ function GetAndChangeAllPhotosStatus(status)
   // get the child div, which is the one that contains all the divs for photos
   var photos = GetPhotosLoaded(photoContainer);
   console.log("Found " + photos.length + " photos");
+  // Grab the users ID.
   config.userId = GetUsersId();
   var i;
   for (i = 0; i < photos.length; i++) { 
-    // The container is becasue if we don't create a new variable, this will be shared among all instances of setTimeout.
+    // The container is for creating a new variable, if you don't do this, it's going to share it among all instances of the function.
     SetPhotoStatusContainer(photos[i], status, config.timeBetweenLoadingPhotosMS * i);
   };
 };
@@ -36,7 +37,8 @@ function GetUsersId()
 {
    var metas = document.getElementsByTagName('meta'); 
 
-   for (var i=0; i<metas.length; i++) { 
+   for (var i=0; i < metas.length; i++) { 
+     // We're going to grab the users id by getting the android url. This is just a simple meta tag.
       if (metas[i].getAttribute("property") == "al:android:url") { 
          return metas[i].getAttribute("content").substr(13);
       } 
@@ -48,31 +50,6 @@ function GetPhotosLoaded(container)
   return container.children;
 }
 
-function ClickOnButton(statusButtonDiv)
-{
-  // Generic method as we make may make this more generic in future by reverse engineering the scripts involved
-    statusButtonDiv.click();
-}
-
-function GetPrivateButtonDiv()
-{
-  var divs = document.getElementsByTagName("div");
-  var elementText = "Only me";
-  var divObject;
-
-  for (var i = 0; i < divs.length; i++) {
-    if (divs[i].textContent == elementText) {
-      divObject = divs[i];
-      break;
-    }
-  }
-  // Go up the treeeeee until we find the root list element
-  divObject = divObject.parentElement.parentElement.parentElement;
-  console.log("Found button to set to private");
-  console.log(divObject);
-  return divObject;
-}
-
 function SetPhotoStatusContainer(photoDiv, status, time)
 {
   setTimeout(function() {SetPhotoStatus(photoDiv, status)}, time);
@@ -82,8 +59,10 @@ function SetPhotoStatus(photoDiv, status)
 {
   console.log("Loading photo div");
   console.log(photoDiv);
+  // Load up the DIV, as the private fbid doesn't load unless we do this. TODO: Figure out how private photo ID is grabbed, as the HTTP requests do not expose this. The public photo ID will return an error, so this is a requirement at this stage.
   photoDiv.children[1].click();
-  // Send post event to facebook
+  // Send post event to facebook, as button presses will not work from album overview.
+  // needs a delay, since the div may not yet be loaded, this can be optimized (only needs to be done on the first div load)
   setTimeout(function() {SendSetPrivateEvent(photoDiv, status)}, 500);
 }
 
@@ -91,23 +70,24 @@ function SendSetPrivateEvent(photoDiv, status)
 {
   console.log("id");
   var privacyPhotoId = document.getElementsByName("ft_ent_identifier")[1].value;
+  // This is a field that's required for the post request. It's one of the only ones required, other than the userid. All the others(about 10 of them) do not produce any different results upon replication.
   var dtsg = document.getElementsByName('fb_dtsg')[0].value;;
   console.log("Privacy photo id is: " + privacyPhotoId);
   
   var http = new XMLHttpRequest();
-  var url = "/privacy/selector/update/" + "?privacy_fbid=PHOTOID&post_param=286958161406148&render_location_enum=stream&is_saved_on_select=true&should_return_tooltip=false&prefix_tooltip_with_app_privacy=false&replace_on_select=false&ent_id=PHOTOID&dpr=1.".replace("PHOTOID", privacyPhotoId).replace("PRIVACYTYPE", "286958161406148");;
+  var url = "/privacy/selector/update/" + "?privacy_fbid=PHOTOID&post_param=286958161406148&render_location_enum=stream&is_saved_on_select=true&should_return_tooltip=false&prefix_tooltip_with_app_privacy=false&replace_on_select=false&ent_id=PHOTOID&dpr=1.".replace("PHOTOID", privacyPhotoId);
   var params = "__user=" + config.userId + "&__a=1&fb_dtsg=" + dtsg;
   http.open("POST", url, true);
 
   http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-  http.onreadystatechange = function() {//Call a function when the state changes.
+  http.onreadystatechange = function() {
       if(http.readyState == 4 && http.status == 200) {
           console.log(http.responseText);
       }
   }
   http.send(params);
-  console.log("Sent!");
+  console.log("Sent request!");
 }
-GetAndChangeAllPhotosStatus(config.targetStatus);
 
+GetAndChangeAllPhotosStatus(config.targetStatus);
